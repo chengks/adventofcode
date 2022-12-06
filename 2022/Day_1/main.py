4ff434c7f7220f41331a49ba41106aa41c2d3ef7
chengks <kim.sing.cheng@gmail.com>
2022-12-06 12:09:54 +0100

    implemented day 1 part 2 and day 6 part 1 and part 2

--- 2022/Day_1/main.py
+++ 2022/Day_1/main.py
@@ -2,11 +2,14 @@ with open("input.txt") as file_in:
     number = []
     answer = 0
     current = 0
+    l = []
     for line in file_in:
         if(line == "\n"):
             answer = current if current > answer else answer
+            l.append(current)
             current = 0
             continue
         current += int(line)
-        
+    sorted_list = sorted(l, reverse=True)
 print(answer)
+print(sorted_list[0] + sorted_list[1] + sorted_list[2])
---
+++ 2022/Day_6/main_part1.py
@@ -0,0 +1,25 @@
+with open("input.txt") as file:
+    input = ""
+    while (line := file.readline().rstrip()):
+        input = line
+    
+    answer = 0
+    for idx, s in enumerate(input):
+        pos = 1
+        duplicate = False       
+        sub_list = [s]
+        while pos < 4 and idx + pos < len(input):
+            sub_list.append(input[idx + pos])
+            pos += 1
+
+        for idx2,c in enumerate(sub_list):
+            duplicate = any([c in c2 for c2 in sub_list[idx2+1::]])
+            if duplicate:
+                break;
+            
+            
+        if not duplicate:
+            answer = idx + 4
+            break;
+    
+    print(answer)    
